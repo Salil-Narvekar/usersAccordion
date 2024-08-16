d01d4aeccadaa2bc49e52bfeb015e70c3ad5f284
Salil Narvekar <salil.enpointe@gmail.com>
2024-08-16 20:48:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  build: {
-    outDir: 'build',
-  },
+  base: "/userAccordion/",
+  // build: {
+  //   outDir: 'build',
+  // },
 })
